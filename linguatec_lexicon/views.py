a75250308b955a79cac4df596835f7e657e4fce7
Santiago Lamora <santiago@pangea.org>
2019-01-04 13:34:43 +0100

    Implement basic readonly API of Word model.

--- linguatec_lexicon/views.py
+++ linguatec_lexicon/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Word
+from .serializers import WordSerializer
+
+
+class WordViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint that allows words to be viewed.
+    """
+    queryset = Word.objects.all().order_by('term')
+    serializer_class = WordSerializer
