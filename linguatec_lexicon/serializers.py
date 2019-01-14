f7a50d583398336f39af15a79ac3d4b87e152bea
Santiago Lamora <santiago@pangea.org>
2019-01-14 12:59:50 +0100

    Show full word content (nested serializers).

--- linguatec_lexicon/serializers.py
+++ linguatec_lexicon/serializers.py
@@ -1,9 +1,25 @@
 from rest_framework import serializers
 
-from .models import Word
+from .models import Entry, Example, Word
 
 
-class WordSerializer(serializers.HyperlinkedModelSerializer):
+class ExampleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Example
+        fields = ('phrase',)
+
+
+class EntrySerializer(serializers.ModelSerializer):
+    examples = ExampleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Entry
+        fields = ('translation', 'examples')
+
+
+class WordSerializer(serializers.ModelSerializer):
+    entries = EntrySerializer(many=True, read_only=True)
+
     class Meta:
         model = Word
-        fields = ('url', 'term', 'gramcat')
+        fields = ('url', 'term', 'gramcat', 'entries')
