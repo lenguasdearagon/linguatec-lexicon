c9d38bdff9d6bd39f76eb195b1785a20d39b54d4
Santiago Lamora <santiago@ribaguifi.com>
2019-01-30 12:31:31 +0100

    Implement view of data-validator.

--- linguatec_lexicon/urls.py
+++ linguatec_lexicon/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url, include
+from django.conf.urls import include, url
+from django.urls import path
 from rest_framework import routers
 from . import views
 
@@ -9,4 +10,5 @@ router.register(r'words', views.WordViewSet)
 # Wire up our API using automatic URL routing.
 urlpatterns = [
     url(r'^', include(router.urls)),
+    path('validator/', views.DataValidatorView.as_view(), name='validator')
 ]
