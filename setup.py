5dd43dded021eaf13462228068b99a0c1e555251
Santiago Lamora <santiago.lamora@gmail.com>
2019-02-01 11:33:27 +0100

    Fix project URL.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name="linguatec-lexicon",
     version=version,
-    url = 'https://gitlab.com/slamora/linguatec-lexicon/',
+    url = 'https://gitlab.com/linguatec/linguatec-lexicon/',
     author = 'Santiago Lamora',
     author_email='santiago@ribaguifi.com',
     description = ('An online bilingual dictionary based on Django.'),
