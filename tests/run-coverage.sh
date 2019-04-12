4e31400de60e08fc863101779a86ce0eff2d639a
Santiago Lamora <slamora@users.noreply.github.com>
2019-04-12 10:52:02 +0200

    Bugfix: coverage cannot be run on parallel.

--- tests/run-coverage.sh
+++ tests/run-coverage.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
-sitepath='/path/to/mysite/'
-
-envpath='/path/to/env/bin/activate'
+envpath='/root/linguatec-sonarqube/projects/env/bin/activate'
 . "$envpath"
+sourcepath='/path/to/linguatec-lexicon/linguatec_lexicon'
 
-cd "$sitepath"
+cd "$sourcepath"
 coverage erase
-coverage run --source '.,/path/to/linguatec-lexicon/linguatec_lexicon' manage.py test linguatec_lexicon
+coverage run --source "$sourcepath" runtests.py --parallel 1
 # debug
 # coverage report
 coverage xml -i
