04e995e6dfda54233782961aaebd3f993e799832
AlejandroRG31 <alejandro-31-00@hotmail.com>
2020-11-30 11:09:54 -0800

    Moved admin urls from frontend to backend in order to have it in the project

--- linguatec_lexicon/urls.py
+++ linguatec_lexicon/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.urls import path
 from rest_framework import routers
 from . import views
+from django.contrib import admin
 
 
 router = routers.DefaultRouter()
@@ -14,4 +15,5 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     path('validator/', views.DataValidatorView.as_view(), name='validator'),
     path('validator-diatopic-variation/', views.DiatopicVariationValidatorView.as_view(), name='validator-variation'),
+    path('admin/', admin.site.urls, name='index'),
 ]
