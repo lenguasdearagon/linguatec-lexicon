e80de35fd33be03ff5a591f75577abd3bee975b1
Santiago Lamora <santiago@ribaguifi.com>
2020-12-09 14:25:06 +0100

    Add admin url to tests.urls

--- tests/urls.py
+++ tests/urls.py
@@ -3,9 +3,11 @@ URLConf for test suite.
 
 """
 from django.conf.urls import include
+from django.contrib import admin
 from django.urls import path
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/', include('linguatec_lexicon.urls')),
 ]
