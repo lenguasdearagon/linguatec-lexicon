66d03ace15b969fad7e98db1d625414d7e2ef028
Santiago Lamora <santiago@ribaguifi.com>
2020-12-09 14:19:57 +0100

    Move `admin` from app to project URLs.

--- linguatec_lexicon/urls.py
+++ linguatec_lexicon/urls.py
@@ -2,18 +2,15 @@ from django.conf.urls import include, url
 from django.urls import path
 from rest_framework import routers
 from . import views
-from django.contrib import admin
 
-
+# Wire up our API using automatic URL routing.
 router = routers.DefaultRouter()
 router.register(r'lexicons', views.LexiconViewSet)
 router.register(r'words', views.WordViewSet)
 router.register(r'gramcats', views.GramaticalCategoryViewSet)
 
-# Wire up our API using automatic URL routing.
 urlpatterns = [
     url(r'^', include(router.urls)),
     path('validator/', views.DataValidatorView.as_view(), name='validator'),
     path('validator-diatopic-variation/', views.DiatopicVariationValidatorView.as_view(), name='validator-variation'),
-    path('admin/', admin.site.urls, name='index'),
 ]
