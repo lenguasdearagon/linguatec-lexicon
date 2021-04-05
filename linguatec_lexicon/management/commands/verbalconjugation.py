90c733d1c48007a2cd0955c8820b9d7e5f7ee06f
Cayo Puigdefabregas <cayo@ribaguifi.com>
2021-04-05 22:32:37 +0200

    adding verbalconjugation as command

--- linguatec_lexicon/management/commands/verbalconjugation.py
+++ linguatec_lexicon/management/commands/verbalconjugation.py
@@ -9,12 +9,13 @@ class Command(BaseCommand):
         for k_entry, v_raw in Entry.words_conjugation().items():
             for entry in Entry.objects.filter(word__term=k_entry, word__lexicon__src_language='es'):
                 verbs = VerbalConjugation.objects.filter(entry=entry)
+                words = [v for v in v_raw if v in entry.translation]
                 if verbs:
                     verb = verbs.first()
-                    verb.raw_verbs = v_raw
+                    verb.raw_verbs = words
                     verb.save()
                 else:
                     VerbalConjugation.objects.create(
                         entry=entry,
-                        raw_verbs=v_raw
+                        raw_verbs=words
                     )
