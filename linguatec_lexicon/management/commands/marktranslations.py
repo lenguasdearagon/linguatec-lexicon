266c84fa9488c0c35ab59e6d1ec0bba78dd7ea47
Santiago Lamora <santiago@ribaguifi.com>
2021-08-20 15:07:51 +0200

    Combine method & re to mark translations

--- linguatec_lexicon/management/commands/marktranslations.py
+++ linguatec_lexicon/management/commands/marktranslations.py
@@ -1,15 +1,19 @@
 import re
+from django.core.exceptions import ValidationError
 
 from django.core.management.base import BaseCommand
 from django.db import transaction
+from django.utils.functional import cached_property
+
 from linguatec_lexicon.models import Entry, Lexicon
+from linguatec_lexicon.validators import validate_balanced_parenthesis
 
 
 class Command(BaseCommand):
     help = 'Fill marked_translation field to link words'
     default_batch_size = 100
     # words including dash '-' and slash '/' not surrounded by parenthesis '()'
-    regex = r'((?<!\()\b\S+\b(?![\w\s]*[\)]))'
+    regex = r'(?<!\()\b\S+\b(?![\w\s]*[\)])'
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -30,15 +34,16 @@ class Command(BaseCommand):
         # TODO(@slamora): handle if there is not reverse pair
         self.lex_reverse = lexicon.get_reverse_pair()
 
-        # cache lexicon words on a set (hash lookup has better performance)
-        self.lex_words = set(self.lex_reverse.words.values_list('term', flat=True))
-
         entries = []
         qs = Entry.objects.filter(word__lexicon=lexicon)
         for entry in qs:
-            marked_translation = re.sub(
-                self.regex,
-                self.mark_word, entry.translation)
+            try:
+                marked_translation = self.mark_text(entry.translation)
+            except ValidationError:
+                variation_name = entry.variation.name if entry.variation else ''
+                msg = "[{}@{}] '{}' Unbalanced parenthesis on entry '{}'".format(
+                    lexicon.code, variation_name, entry.word.term, entry.translation)
+                self.stdout.write(self.style.ERROR(msg))
             if "</trans>" in marked_translation:
                 entry.marked_translation = marked_translation
                 entries.append(entry)
@@ -47,9 +52,49 @@ class Command(BaseCommand):
 
         return len(entries), qs.count()
 
+    def mark_text(self, text):
+        text_marked = ''
+        for chunk in split_by_parenthesis(text):
+            if not chunk.startswith('('):
+                chunk = re.sub(self.regex, self.mark_word, chunk)
+            text_marked += chunk
+
+        return text_marked
+
     def mark_word(self, matchobj):
-        match_chunk = matchobj.group(1)
+        match_chunk = matchobj.group(0)
         translation_exists = match_chunk in self.lex_words
         if translation_exists:
             return "<trans lex=" + self.lex_reverse.code + ">" + match_chunk + "</trans>"
         return match_chunk
+
+    @cached_property
+    def lex_words(self):
+        # cache lexicon words on a set (hash lookup has better performance)
+        return set(self.lex_reverse.words.values_list('term', flat=True))
+
+
+def split_by_parenthesis(text):
+    validate_balanced_parenthesis(text)
+    groups = []
+    start = 0
+    while True:
+        end = text.find('(', start)
+        if end >= 0:
+            chunk = text[start:end]
+            if chunk:
+                groups.append(chunk)
+                start = end
+
+            # find closing parenthesis
+            end = text.find(')', end) + 1
+            groups.append(text[start:end])
+            start = end
+
+        else:
+            chunk = text[start:]
+            if chunk:
+                groups.append(chunk)
+            break
+
+    return groups
--- tests/test_marktranslations.py
+++ tests/test_marktranslations.py
@@ -1,4 +1,6 @@
 import re
+import unittest
+from django.core.exceptions import ValidationError
 
 from django.test import TestCase
 from linguatec_lexicon.management.commands import marktranslations
@@ -26,8 +28,40 @@ class MarkTranslationRegex(TestCase):
     def test_exclude_words_surrounded_by_parenthesis(self):
         self.assertRegexpFindAll('(cúmulo) boira grasa', ['boira', 'grasa'])
 
+    @unittest.expectedFailure
     def test_exclude_words_surrounded_by_parenthesis_and_commas(self):
         self.assertRegexpFindAll(
             'boira (Puede emplearse con cuantificadores: dos boiras, bellas boiras, muitas boiras)',
             ['boira']
         )
+
+
+class MarkTranslationMethod(TestCase):
+    def assertMethodSplit(self, text, expected):
+        result = marktranslations.split_by_parenthesis(text)
+        self.assertListEqual(expected, result)
+
+    def test_single_word(self):
+        self.assertMethodSplit('casa', ['casa'])
+
+    def test_several_words_comma_separated(self):
+        self.assertMethodSplit('zerpeta, boira seca', ['zerpeta, boira seca'])
+
+    def test_words_with_dash(self):
+        self.assertMethodSplit('pentinar-se', ['pentinar-se'])
+
+    def test_words_with_slash(self):
+        self.assertMethodSplit('casto/a', ['casto/a'])
+
+    def test_exclude_words_surrounded_by_parenthesis(self):
+        self.assertMethodSplit('(cúmulo) boira grasa', ['(cúmulo)', ' boira grasa'])
+
+    def test_exclude_words_surrounded_by_parenthesis_and_commas(self):
+        self.assertMethodSplit(
+            'boira (Puede emplearse con cuantificadores: dos boiras, bellas boiras, muitas boiras) caraca, (has)',
+            ['boira ', '(Puede emplearse con cuantificadores: dos boiras, bellas boiras, muitas boiras)',
+             ' caraca, ', '(has)']
+        )
+
+    def test_invalid_unbalanced_parenthesis(self):
+        self.assertRaises(ValidationError, marktranslations.split_by_parenthesis, '(')
